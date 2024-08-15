a24f6ed97c47ca70ebab1b359e32a28fc59ceda7
FrontMD <web@mockup.digital>
2024-08-15 12:59:44 +0300

    home desk in origress

--- src/blocks/modules/modals/modals.js
+++
@@ -1,3 +0,0 @@
-const modals = new HystModal({
-    linkAttributeName: "data-hystmodal",
-});
--- src/js/import/modules.js
+++
@@ -1,7 +0,0 @@
-@@include("../../blocks/modules/header/header.js")
-@@include("../../blocks/modules/modals/modals.js")
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    //headerControllInit();
-})
